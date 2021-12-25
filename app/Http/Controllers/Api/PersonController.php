7e2de85907e928ef6d52abb9de8206e6bec94e79
dhanaraj <dhanraj@propelsoft.in>
2021-12-25 17:03:08 +0530

    sample

--- app/Http/Controllers/Api/PersonController.php
+++ app/Http/Controllers/Api/PersonController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Models\PersonRelated\Person;
 class PersonController extends Controller
 {
-    public function index(){
-    
+    public function index()
+    {
+        $model = Person::get();
+    //     dd($model);
+    // dd("well dr");
         return view('MajorScreens.person');
     }
 }
---
+++ app/Models/PersonRelated/Person.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\PersonRelated;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Person extends Model
+{
+    protected $table = 'persons';
+    use HasFactory;
+}
